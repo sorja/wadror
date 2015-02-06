d93891c90c94ed5c6e05ffb14d6fead7ad664607
Miró Sorja <--global>
2015-02-06 17:20:26 +0200

    Updated navi

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
-
+  belongs_to :user # belongs to user
   def to_s
     "#{beer.name} #{score}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150201162921) do
+ActiveRecord::Schema.define(version: 20150206131145) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150201162921) do
     t.integer  "beer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
